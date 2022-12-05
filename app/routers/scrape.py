7056a51c441e0b75f1ee5921e11ba99629b0efad
0sahil <sk5237707@gmail.com>
2022-12-05 16:27:14 +0530

    Change response schema v2

--- app/routers/scrape.py
+++ app/routers/scrape.py
@@ -12,25 +12,25 @@ async def get_prices(
     try:
         flipkart_data, amazon_data = get_price(product)
 
-        flipkart_data_ = []
-        amazon_data_ = []
+        flipkart_data_list = []
+        amazon_data_list = []
 
         for each in flipkart_data:
-            flipkart_data_.append({
+            flipkart_data_list.append({
                 'name': each,
                 'price': flipkart_data[each][0],
                 'link': flipkart_data[each][1]
             })
         for each in amazon_data:
-            amazon_data_.append({
+            amazon_data_list.append({
                 'name': each,
                 'price': amazon_data[each][0],
                 'link': amazon_data[each][1]
             })
 
         data = {
-            'flipkart': flipkart_data_,
-            'amazon': amazon_data_
+            'flipkart': flipkart_data_list,
+            'amazon': amazon_data_list
         }
         return {'message': 'success', 'data': data}
     except Exception as e:
